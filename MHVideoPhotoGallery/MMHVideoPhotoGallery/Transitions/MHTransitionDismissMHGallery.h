f07215ed17a8d293be6c0122dc4fdee2938e9399
Jiangjinfu <jinfu.jiang@kankanapp.com.cn>
2017-01-13 11:36:17 +0800

    update dismissing animation bg colour

--- MHVideoPhotoGallery/MMHVideoPhotoGallery/Transitions/MHTransitionDismissMHGallery.h
+++ MHVideoPhotoGallery/MMHVideoPhotoGallery/Transitions/MHTransitionDismissMHGallery.h
@@ -21,15 +21,3 @@
 
 @property (nonatomic,assign)    id <UIViewControllerContextTransitioning> context;
 @end
-
-@interface MHTransitionDismissDefault : UIPercentDrivenInteractiveTransition<UIViewControllerAnimatedTransitioning>
-
-@property (nonatomic,strong)    MPMoviePlayerController *moviePlayer;
-//@property (nonatomic,strong)    UIImageView *transitionImageView;
-@property (nonatomic,assign)    CGPoint changedPoint;
-@property (nonatomic,assign)    CGFloat orientationTransformBeforeDismiss;
-@property (nonatomic,assign)    BOOL interactive;
-@property (nonatomic,assign)    BOOL finishButtonAction;
-
-@property (nonatomic,assign)    id <UIViewControllerContextTransitioning> context;
-@end
